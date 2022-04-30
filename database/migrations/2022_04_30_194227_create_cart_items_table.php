963e8f45a0e5cf9b6533ba37029ccd7112db846e
Tapiwa Tererai <tapiwatererai@outlook.com>
2022-04-30 22:31:45 +0200

    corrections

--- database/migrations/2022_04_30_194227_create_cart_items_table.php
+++ database/migrations/2022_04_30_194227_create_cart_items_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('cart_item_id');
-            $table->foreign('cart_item_id')->references('id')->on('carts')->onDelete('cascade');
+            $table->unsignedBigInteger('cart_id');
+            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
